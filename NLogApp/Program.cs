d3e7c7c2cd93e9ac00c74abd57f9ee921119a716
frankreashore <frank.reashore@hyperviewhq.com>
2020-03-11 16:47:11 -0700

    Add project files.

---
+++ NLogApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace NLogApp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+
+            try
+            {
+                IConfigurationRoot configurationBuilder = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory()) //From NuGet Package Microsoft.Extensions.Configuration.Json
+                   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                   .Build();
+
+                IServiceProvider servicesProvider = BuildDi(configurationBuilder);
+
+                using (servicesProvider as IDisposable)
+                {
+                    Runner runner = servicesProvider.GetRequiredService<Runner>();
+                    runner.DoAction("Action1");
+
+                    string message = "Press any key to exit";
+                    Console.WriteLine(message);
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception exception)
+            {
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
+        }
+
+        private static IServiceProvider BuildDi(IConfiguration configuration)
+        {
+            return new ServiceCollection()
+               .AddTransient<Runner>() // Runner is the custom class
+               .AddLogging(loggingBuilder =>
+               {
+                   // configure Logging with NLog
+                   loggingBuilder.ClearProviders();
+                   loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                   // obsolete
+                   loggingBuilder.AddNLog(configuration);
+               })
+               .BuildServiceProvider();
+        }
+    }
+}
---
+++ NLogApp/Runner.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NLogApp
+{
+    public class Runner
+    {
+        private readonly ILogger<Runner> _logger;
+
+        public Runner(ILogger<Runner> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoAction(string name)
+        {
+            const string message = "Doing hard work! {Action}";
+            _logger.LogDebug(20, message, name);
+        }
+    }
+}
